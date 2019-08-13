b0d142fb6ab85c69c18a8485c8c55d7f798cb89a
Subash Pradahan <suprowilde5@gmail.com>
2019-08-13 13:55:15 +0200

    Adding for heroku database

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
-const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
+// const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
+const databaseUrl = process.env.DATABASE_URL || "https://calm-reef-14417.herokuapp.com"
 const db = new Sequelize(databaseUrl)
 
 db.sync({force: false})
